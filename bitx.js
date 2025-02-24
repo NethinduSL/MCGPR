acb93c21afaf61826a83a84468a476b6f90388a0
NethinduSL <144025781+NethinduSL@users.noreply.github.com>
2025-02-24 21:35:54 +0530

    Update bitx.js

--- bitx.js
+++ bitx.js
@@ -6,9 +6,9 @@ document.addEventListener("DOMContentLoaded", () => {
             document.body.classList.toggle("dark-mode");
 
             if (document.body.classList.contains("dark-mode")) {
-                toggleBtn.innerText = "☀️ Light Mode";
+                toggleBtn.innerText = "☀️";
             } else {
-                toggleBtn.innerText = "🌙 Dark Mode";
+                toggleBtn.innerText = "🌙";
             }
         });
     }
